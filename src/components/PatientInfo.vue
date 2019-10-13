689fbdcc82d394562e697b06e85b99a6df97d3e9
A McArdle <38906087+amcardle@users.noreply.github.com>
2019-10-13 16:38:52 +0100

    Remove unused tools

--- src/components/PatientInfo.vue
+++ src/components/PatientInfo.vue
@@ -3,13 +3,6 @@
   b-card(title='Patient Details')
     .container
       .row
-        .col-sm-4
-          b-input-group.mb-2(size='s', prepend='ID')
-            b-form-input(v-model='id')
-        .col-sm
-          b-input-group.mb-2(size='s', prepend='Name')
-            b-form-input(v-model='name')
-      .row
         .col-sm
           b-input-group.mb-2(size='s', prepend='DOB')
             b-form-input(v-model='dob', type='date')
@@ -41,22 +34,6 @@
               b-card
                 .card-text
                   b-table(responsive='', :items='asa_grades')
-      .row
-        .col-sm
-          b-button-group
-            b-button(:pressed='surgical_grade == 1', v-on:click='surgical_grade=1') Minor
-            b-button(:pressed='surgical_grade == 2', v-on:click='surgical_grade=2') Intermediate
-            b-button(:pressed='surgical_grade == 3', v-on:click='surgical_grade=3') Major
-            b-btn(v-b-toggle.collapse2='', variant='primary') ?
-          div
-            b-collapse#collapse2.mt-2
-              b-card
-                .card-text
-                  b-table(responsive='', :items='surgical_grades')
-      .row
-        .col-sm
-          b-form-group(label='Comorbidities')
-            b-form-checkbox-group(buttons='', v-model='comorbidities', stacked='', :options='comorbidity_options')
 
 </template>
 
@@ -64,20 +41,17 @@
 import { mapState } from 'vuex'
 import { sync, get } from 'vuex-pathify';
 import asa_grades from '../reference/asa_grades'
-import surgical_grades from '../reference/surgical_grades'
-import comorbidity_options from '../reference/comorbidities'
 
 export default {
   name: "PatientInfo",
 
   data: function () {
     return {      asa_grades: asa_grades,
-      comorbidity_options: comorbidity_options,
-      surgical_grades: surgical_grades    }
+
+    }
   },
   computed: {
-    id: sync('patient/id'),
-    name: sync('patient/name'),
+
     dob: sync('patient/dob'),
     height: sync("patient/height"),
     weight: sync("patient/weight"),
@@ -85,8 +59,6 @@ export default {
     bmi: get('patient/bmi'),
     bmi_category: get('patient/bmi_category'),
     asa: sync('patient/asa'),
-    surgical_grade: sync('patient/surgical_grade'),
-    comorbidities: sync('patient/comorbidities'),
     IBW: get('patient/IBW')
   },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,23 +3,18 @@
 <template lang="pug">
 .home
   PatientInfo
-  Medications
-  IntubationDifficulty
-  Investigations
+  
+  
+  
   ReferenceValues
   DrugDoses
-  ChecklistWHO
 </template>
 
 <script>
 import PatientInfo from "@/components/PatientInfo.vue";
 import DrugDoses from "@/components/DrugDoses.vue";
-import Investigations from "@/components/Investigations.vue";
 import ReferenceValues from "@/components/ReferenceValues.vue";
-import IntubationDifficulty from "@/components/IntubationDifficulty.vue"
 import OpiateDoseConverter from "@/components/OpiateDoseConverter.vue"
-import Medications from "@/components/Medications.vue"
-import ChecklistWHO from "@/components/ChecklistWHO.vue"
 
 
 export default {
@@ -27,12 +22,8 @@ export default {
   components: {
     PatientInfo,
     DrugDoses,
-    Investigations,
     ReferenceValues,
-    IntubationDifficulty,
     OpiateDoseConverter,
-    Medications,
-    ChecklistWHO
   }
 };
 </script>
