2c392e8ebc019069ceb48464ce796e50eda306e7
A McArdle <38906087+amcardle@users.noreply.github.com>
2019-10-13 19:02:45 +0100

    More drug doses

--- src/components/drugs/AllDrugs.js
+++ src/components/drugs/AllDrugs.js
@@ -3,7 +3,11 @@ import Thiopental from './Thiopental.vue'
 import Fentanyl from './Fentanyl.vue'
 import Alfentanil from './Alfentanil.vue'
 import Morphine from './Morphine.vue'
+import Atracurium from './Atracurium.vue'
+import Vecuronium from './Vecuronium.vue'
+import Rocuronium from './Rocuronium.vue'
+import Suxamethonium from './Suxamethonium.vue'
 
-const drugs = [Propofol, Thiopental, Fentanyl, Alfentanil, Morphine]
+const drugs = [Propofol, Thiopental, Fentanyl, Alfentanil, Morphine, Atracurium, Vecuronium, Rocuronium, Suxamethonium]
 
 export default drugs
---
+++ src/components/drugs/Atracurium.vue
@@ -0,0 +1,20 @@
+<script>
+
+import DrugDose from './DrugDose.vue'
+
+export default {
+  mixins: [DrugDose],
+  name: "Atracurium",
+  data: function () {
+    return {
+      ///// ADJUST THE FOLLOWING PARAMETERS
+      name: "Atracurium",
+      drug_class: "Muscle Relaxant",
+      dose: "0.5 mg/kg",
+      presentation: "10 mg/ml",
+      color: "background-color:#FF7276"
+    }
+  },
+
+};
+</script>
