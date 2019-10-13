ac57eadfb810b273f697928c741349ce2d4ee1d0
A McArdle <38906087+amcardle@users.noreply.github.com>
2019-10-13 18:56:33 +0100

    Switch to mixins

--- src/components/drugs/Fentanyl.vue
+++ src/components/drugs/Fentanyl.vue
@@ -1,38 +1,22 @@
 <script>
-import { sync, get } from 'vuex-pathify';
-import * as math from 'mathjs';
+
+import DrugDose from './DrugDose.vue'
 
 export default {
   name: "Fentanyl",
+  mixins: [DrugDose],
+
   data: function () {
     return {
-      math: math,
+
       ///// ADJUST THE FOLLOWING PARAMETERS
       name: "Fentanyl",
       drug_class: "Opioid",
       dose: "1 microg/kg",
-      presentation: "50 microg/ml"
+      presentation: "50 microg/ml",
+      color: "background-color:#71C5E8;"
     }
   },
-  computed: {
-    weight: get("patient/weight")
-  },
+
 };
 </script>
-
-<template>
-  <div class="drug" style="background-color:#71C5E8;">
-    <div class="col-3">{{name}}</div>
-
-    <div class="col-3">{{drug_class}}</div>
-
-    <div class="col-3">
-      <div>{{math.format(math.eval(`${dose} * ${weight} kg`), 4)}}</div>
-      <small class="text-muted">{{dose}}</small>
-    </div>
-    <div class="col-3">
-      <div>{{math.format(math.eval(`(${dose} * ${weight} kg)/(${presentation})`), 4)}}</div>
-      <small class="text-muted">{{presentation}}</small>
-    </div>
-  </div>
-</template>
--- src/components/drugs/Thiopental.vue
+++ src/components/drugs/Thiopental.vue
@@ -1,39 +1,21 @@
 <script>
-import { sync, get } from 'vuex-pathify';
-import * as math from 'mathjs';
+import DrugDoseRange from './DrugDoseRange.vue'
 
 export default {
-  name: "Propofol",
+  name: "Thiopental",
+  mixins: [DrugDoseRange],
+
   data: function () {
     return {
-      math: math,
       ///// ADJUST THE FOLLOWING PARAMETERS
       name: "Thiopental",
       drug_class: "Induction Agent",
       lowest_dose: "3 mg/kg",
       highest_dose: "5 mg/kg",
-      presentation: "25 mg/ml"
+      presentation: "25 mg/ml",
+      color: "background-color:Yellow;"
     }
-  },
-  computed: {
-    weight: get("patient/weight")
-  },
+  }
 };
 </script>
 
-<template>
-  <div class="drug" style="background-color:yellow">
-    <div class="col-3">{{name}}</div>
-
-    <div class="col-3">{{drug_class}}</div>
-
-    <div class="col-3">
-      <div>{{math.format(math.eval(`${lowest_dose} * ${weight} kg`), 4)}} - {{math.format(math.eval(`${highest_dose} * ${weight} kg`), 4)}}</div>
-      <small class="text-muted">{{lowest_dose}} - {{highest_dose}}</small>
-    </div>
-    <div class="col-3">
-      <div>{{math.format(math.eval(`(${lowest_dose} * ${weight} kg)/(${presentation})`), 4)}} - {{math.format(math.eval(`${highest_dose} * ${weight} kg/(${presentation})`), 4)}}</div>
-      <small class="text-muted">{{presentation}}</small>
-    </div>
-  </div>
-</template>
