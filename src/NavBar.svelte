5856255e32855f4f5801b0eae8bc4c3c3a0d4ca2
A McArdle <38906087+amcardle@users.noreply.github.com>
2019-10-20 17:42:08 +0100

    Add link to GitHub 'readme' file on about page

--- src/NavBar.svelte
+++ src/NavBar.svelte
@@ -1,4 +1,14 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
   <a class="navbar-brand" href="#">AAPP</a>
+  <ul class="navbar-nav mr-auto">
 
+    <li class="nav-item">
+      <a
+        class="nav-link"
+        href="https://github.com/amcardle/aapp/blob/master/README.md">
+        About
+      </a>
+    </li>
+
+  </ul>
 </nav>
