c14902a09e2250b78ffe30244bc4b71f41528b5a
A McArdle <38906087+amcardle@users.noreply.github.com>
2019-10-20 12:25:55 +0100

    import all bulma

--- src/App.svelte
+++ src/App.svelte
@@ -3,19 +3,4 @@
   export let name;
 </script>
 
-<style>
-  h1 {
-    color: purple;
-  }
-</style>
-
-<h1>Hello {name}!</h1>
-
-<div class="columns">
-  <div class="column">1</div>
-  <div class="column">2</div>
-  <div class="column">3</div>
-  <div class="column">4</div>
-  <div class="column">5</div>
-</div>
-<button class="button is-primary">hello</button>
+{name}
