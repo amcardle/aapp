36a1f59e06f32c9edf548792edc4bcbd2d027726
A McArdle <38906087+amcardle@users.noreply.github.com>
2019-10-27 19:59:32 +0000

    Switch drugs to LBW if indicated by BJA article

--- src/drugs/Alfentanil.svelte
+++ src/drugs/Alfentanil.svelte
@@ -1,14 +1,16 @@
 <script>
   import DrugComponent from "./DrugComponent.svelte";
-  import { weight, height } from "../stores";
+  import { weight, height, lean_body_weight } from "../stores";
 </script>
 
 <DrugComponent drugClass="opioid">
   <span slot="drugName">Alfentanil</span>
 
-  <span slot="drugDose">{(10 * $weight).toFixed(1)} microg (10 microg/kg)</span>
+  <span slot="drugDose">
+    {(10 * $lean_body_weight).toFixed(1)} microg (10 microg/kg)
+  </span>
 
   <span slot="drugPresentation">
-    {((10 * $weight) / 500).toFixed(1)} ml (500 microg/ml)
+    {((10 * $lean_body_weight) / 500).toFixed(1)} ml (500 microg/ml)
   </span>
 </DrugComponent>
--- src/drugs/Fentanyl.svelte
+++ src/drugs/Fentanyl.svelte
@@ -1,14 +1,16 @@
 <script>
   import DrugComponent from "./DrugComponent.svelte";
-  import { weight, height } from "../stores";
+  import { weight, height, lean_body_weight } from "../stores";
 </script>
 
 <DrugComponent drugClass="opioid">
   <span slot="drugName">Fentanyl</span>
 
-  <span slot="drugDose">{(1 * $weight).toFixed(1)} microg (1 microg/kg)</span>
+  <span slot="drugDose">
+    {(1 * $lean_body_weight).toFixed(1)} microg (1 microg/kg)
+  </span>
 
   <span slot="drugPresentation">
-    {((1 * $weight) / 50).toFixed(1)} ml (50 microg/ml)
+    {((1 * $lean_body_weight) / 50).toFixed(1)} ml (50 microg/ml)
   </span>
 </DrugComponent>
